73f92bf6d8b98324502f621c094bed5ab0f6c8e7
fourstates <ytakahashi@gmail.com>
2016-02-22 19:33:08 -0800

    new index page template and sass files. fixes on urls,views

--- core/views.py
+++ core/views.py
@@ -10,9 +10,22 @@ from django.utils import timezone
 
 from .models import Voter
 
-class IndexView(generic.DetailView):
+class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
 class ProfileView(generic.DetailView):
     model = Voter
     template_name = 'profile.html'
+
+def enterzip(request, zipcode):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        return render(request, 'polls/detail.html', { 'question': question, 'error_message': "You didn't select a choice.", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data. This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
